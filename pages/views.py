0542cd46b42486ae7d3832485561e43381c9649f
CreamMeatball <pch5445@gmail.com>
2023-10-29 21:41:30 +0900

    index, about page subcommit

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def mainpage(request):
+    return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
